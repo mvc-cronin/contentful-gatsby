e34ff5cb77bcde4eef917cfbd6267b539081c1f0
Michael Colianna <mcolianna@cronin-co.com>
2022-03-21 11:51:56 -0400

    Updates contact page to slot the form into the design

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,27 +3,70 @@ import { graphql } from 'gatsby';
 import { FormiumForm } from '@formium/react';
 import { formium } from '../lib/formium';
 
+import Seo from '../components/seo'
+import Layout from '../components/layout'
+import Hero from '../components/hero'
+import * as styles from '../templates/blog-post.module.css'
+
 export default function FeedbackForm({ data }) {
   const [success, setSuccess] = React.useState(false);
 
   if (success) {
-    return <div>Thank you! Your response has been recorded.</div>;
+    return (
+      <Layout location="contact">
+        <Seo
+          title="Contact"
+          description="Contact Us"
+          image={`http:${data.contentfulAsset.resize.src}`}
+        />
+        <Hero
+          image={data.contentfulAsset.gatsbyImageData}
+          title="Contact"
+          content="Contact Form Below"
+        />
+        <div className={styles.container}>
+          <div className={styles.article}>
+            <div className={styles.body}>
+              <div>Thank you! Your response has been recorded.</div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
   }
 
   return (
-    <FormiumForm
-      data={data.formiumForm}
-      onSubmit={async (values) => {
-        // Send form values to Formium
-        await formium.submitForm('contact', values);
-        setSuccess(true);
-      }}
-    />
+    <Layout location="contact">
+      <Seo
+        title="Contact"
+        description="Contact Us"
+        image={`http:${data.contentfulAsset.resize.src}`}
+      />
+      <Hero
+        image={data.contentfulAsset.gatsbyImageData}
+        title="Contact"
+        content="Contact Form Below"
+      />
+      <div className={styles.container}>
+        <div className={styles.article}>
+          <div className={styles.body}>
+            <FormiumForm
+              data={data.formiumForm}
+              onSubmit={async (values) => {
+                // Send form values to Formium
+                await formium.submitForm('contact', values);
+                setSuccess(true);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 }
 
 export const query = graphql`
-  {
+{
     formiumForm(slug: { eq: "contact" }) {
       id
       createAt
@@ -33,6 +76,12 @@ export const query = graphql`
       slug
       updateAt
       version
+    }
+    contentfulAsset(contentful_id: {eq: "392YZxjPZFefJsv3jkCWpd"}) {
+      gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, width: 1280)
+      resize(height: 630, width: 1200) {
+        src
+      }
     }
   }
 `;
